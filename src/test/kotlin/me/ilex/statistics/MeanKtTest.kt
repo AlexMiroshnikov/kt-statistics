3eb06f2432291d9bed63d066663dbe403d297f14
AlexMiroshnikov <ilexicy@gmail.com>
2021-03-05 23:29:52 +0100

    Extend coverage

--- src/test/kotlin/me/ilex/statistics/MeanKtTest.kt
+++ src/test/kotlin/me/ilex/statistics/MeanKtTest.kt
@@ -20,4 +20,20 @@ internal class MeanKtTest {
     fun `it returns mean - if array of int of length gt 1 is given`() {
         assertEquals(1.5, mean(arrayOf(1, 2)))
     }
+
+    @Test
+    fun `it returns 0 - if empty array of double is passed`() {
+        assertEquals(0.0, mean(emptyArray<Double>()))
+    }
+
+    @Test
+    fun `it returns exact number - if array of double length 1 is given`() {
+        val n = 2.0
+        assertEquals(n.toDouble(), mean(arrayOf(n)))
+    }
+
+    @Test
+    fun `it returns mean - if array of double of length gt 1 is given`() {
+        assertEquals(1.5, mean(arrayOf(1.0, 2.0)))
+    }
 }
