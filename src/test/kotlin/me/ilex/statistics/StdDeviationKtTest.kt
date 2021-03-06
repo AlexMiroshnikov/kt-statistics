3abf8e44213a17f19e481101c557d940f68b7dfd
AlexMiroshnikov <ilexicy@gmail.com>
2021-03-06 13:19:46 +0100

    Lint+format

--- src/test/kotlin/me/ilex/statistics/StdDeviationKtTest.kt
+++ src/test/kotlin/me/ilex/statistics/StdDeviationKtTest.kt
@@ -14,7 +14,6 @@ internal class StdDeviationKtTest {
         assertEquals(0.0, stdDeviation(emptyArray<Double>()))
     }
 
-
     @Test
     fun `it returns std deviation - if array of int of length gt 1 is given`() {
         assertEquals(14.142135623730951, stdDeviation(arrayOf(10, 40, 30, 50, 20)))
