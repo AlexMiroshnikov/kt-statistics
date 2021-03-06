b8a8fa3302b828918b5971d2442dd13db86c6ee2
AlexMiroshnikov <ilexicy@gmail.com>
2021-03-06 12:31:27 +0100

    Extend code coverage

--- src/test/kotlin/me/ilex/statistics/QuartileKtTest.kt
+++ src/test/kotlin/me/ilex/statistics/QuartileKtTest.kt
@@ -6,8 +6,39 @@ import org.junit.jupiter.api.Test
 internal class QuartileKtTest {
 
     companion object {
+        val SEQ_INT_SINGLE = arrayOf(1)
         val SEQ_INT_ODD = arrayOf(3, 7, 8, 5, 12, 14, 21, 13)
         val SEQ_INT_EVEN = SEQ_INT_ODD.plusElement(18)
+    }
+
+    @Test
+    fun `it returns 0 as 1st quartile - if empty array of int is passed`() {
+        assertEquals(0.0, quartile(arrayOf<Int>(), Quartile.FIRST))
+    }
+
+    @Test
+    fun `it returns 0 as 2nd quartile - if empty array of int is passed`() {
+        assertEquals(0.0, quartile(arrayOf<Int>(), Quartile.SECOND))
+    }
+
+    @Test
+    fun `it returns 0 as 3rd quartile - if empty array of int is passed`() {
+        assertEquals(0.0, quartile(arrayOf<Int>(), Quartile.THIRD))
+    }
+
+    @Test
+    fun `it returns exact number as 1st quartile - if array of int of length 1 is given`() {
+        assertEquals(1.0, quartile(SEQ_INT_SINGLE, Quartile.FIRST))
+    }
+
+    @Test
+    fun `it returns exact number as 2nd quartile - if array of int of length 1 is given`() {
+        assertEquals(1.0, quartile(SEQ_INT_SINGLE, Quartile.SECOND))
+    }
+
+    @Test
+    fun `it returns exact number as 3rd quartile - if array of int of length 1 is given`() {
+        assertEquals(1.0, quartile(SEQ_INT_SINGLE, Quartile.THIRD))
     }
 
     @Test
