e6435f94530b9be41580d9bbe152fd8e6b1ab7e6
AlexMiroshnikov <ilexicy@gmail.com>
2021-03-06 12:37:37 +0100

    Support Int and Double

--- src/main/kotlin/me/ilex/statistics/quartile.kt
+++ src/main/kotlin/me/ilex/statistics/quartile.kt
@@ -4,7 +4,7 @@ enum class Quartile {
     FIRST, SECOND, THIRD
 }
 
-fun quartile(seq: Array<Int>, quartileNum: Quartile): Double {
+fun quartile(seq: Array<Double>, quartileNum: Quartile): Double {
     if (seq.isEmpty()) {
         return 0.0
     }
@@ -31,3 +31,5 @@ fun quartile(seq: Array<Int>, quartileNum: Quartile): Double {
 
     return median(seq.sliceArray(startIndex until seq.size))
 }
+
+fun quartile(seq: Array<Int>, quartileNum: Quartile): Double = quartile(seq.map { it.toDouble() }.toTypedArray(), quartileNum)
