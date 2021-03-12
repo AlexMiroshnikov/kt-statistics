2a61d67c9948f23bc94eac7612824058d5aab2d3
AlexMiroshnikov <ilexicy@gmail.com>
2021-03-12 22:32:38 +0100

    Code is fixed, but answer is incorrect

--- src/main/kotlin/me/ilex/statistics/tools/CentralLimitTheorem.kt
+++ src/main/kotlin/me/ilex/statistics/tools/CentralLimitTheorem.kt
@@ -3,9 +3,9 @@ package me.ilex.statistics.tools
 import me.ilex.statistics.distributions.Normal
 import kotlin.math.sqrt
 
-class CentralLimitTheorem(private val expectation: Double, private val variance: Double) {
-    fun predict(n: Double, x: Double): Double {
-        val d = Normal.makeThroughVariance(n * expectation, sqrt(n) * variance)
+class CentralLimitTheorem(private val expectation: Double, private val stdDeviation: Double) {
+    fun probability(n: Double, x: Double): Double {
+        val d = Normal.makeThroughDeviation(n * expectation, sqrt(n) * stdDeviation)
         return d.probabilityDensity(x)
     }
 }
--- src/test/kotlin/me/ilex/statistics/tools/CentralLimitTheoremTest.kt
+++ src/test/kotlin/me/ilex/statistics/tools/CentralLimitTheoremTest.kt
@@ -2,13 +2,19 @@ package me.ilex.statistics.tools
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.math.pow
 
 internal class CentralLimitTheoremTest {
+
+    // Expected Output
+    //
+    //    0.0098
+
     @Test
-    fun `it works`() {
-        val t = CentralLimitTheorem(205.0, 15.0.pow(2.0))
-        val result = t.predict(49.0, 49.0)
-        println(result)
+    fun `probability is calculated properly`() {
+//        val t = CentralLimitTheorem(205.0, 15.0)
+//        assertEquals("0.0002", "%.4f".format(t.probability(49.0, 9800.0)))
+        val t = CentralLimitTheorem(205.0, 15.0)
+//        println("%.4f".format(1 - t.probability(49.0, 9800.0)))
+        println("%.4f".format(t.probability(49.0, 9800.0)))
     }
 }
