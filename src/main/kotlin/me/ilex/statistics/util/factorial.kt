7103d28f9a5998f381f154447c9ceda02d7a3cb7
AlexMiroshnikov <ilexicy@gmail.com>
2021-03-13 16:40:57 +0100

    Extend factorial coverage

--- src/main/kotlin/me/ilex/statistics/util/factorial.kt
+++ src/main/kotlin/me/ilex/statistics/util/factorial.kt
@@ -1,8 +1,13 @@
 package me.ilex.statistics.util
 
 import java.math.BigInteger
+import me.ilex.statistics.exceptions.InvalidArgumentException
 
 fun factorial(num: Int): BigInteger {
+    if (num < 0) {
+        throw InvalidArgumentException("Only positive number can be passed, $num was given")
+    }
+
     var result = BigInteger.ONE
 
     if (num >= 2) {
