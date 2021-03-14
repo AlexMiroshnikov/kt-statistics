864c4dcab3f737ef5e63754b3ebe1716d08cc804
AlexMiroshnikov <ilexicy@gmail.com>
2021-03-14 11:01:07 +0100

    Introduce matrices

---
+++ src/main/kotlin/me/ilex/statistics/util/matrices/Matrix.kt
@@ -0,0 +1,92 @@
+package me.ilex.statistics.util.matrices
+
+import me.ilex.statistics.exceptions.InvalidArgumentException
+
+class Matrix {
+    private var rows: Array<DoubleArray>
+
+    private constructor(rows: Array<DoubleArray>) {
+        this.rows = rows
+    }
+
+    companion object {
+        fun makeFromRows(rows: Array<DoubleArray>): Matrix {
+            validateSize(rows)
+            return Matrix(rows)
+        }
+
+        fun makeFromRows(rows: Array<IntArray>): Matrix {
+            //            validateSize(rows)    // @TODO How to use the same validate size method
+            // without copying it for different types?
+            return Matrix(
+                rows.map { row -> row.map { it.toDouble() }.toDoubleArray() }.toTypedArray()
+            )
+        }
+
+        //        fun makeFromCols(cols: Array<DoubleArray>): Matrix {
+        //            validateSize(cols)
+        //            return Matrix()
+        //        }
+
+        private fun validateSize(items: Array<DoubleArray>) {
+            if (items.isEmpty()) {
+                throw InvalidArgumentException("Passing empty items does not make sense")
+            }
+
+            val rowSize = items.first().size
+
+            for (i in items.indices) {
+                if (items[i].size != rowSize) {
+                    throw InvalidArgumentException(
+                        "All the items should be of the same length $rowSize, item with index $i " +
+                            "is of length ${items[i].size}"
+                    )
+                }
+            }
+        }
+    }
+
+    fun toSingleLine(): String {
+        return rows.joinToString(" ") { it.joinToString(" ") }
+    }
+
+    fun addColumn(col: IntArray): Matrix {
+        if (col.size != rows.size) {
+            throw InvalidArgumentException(
+                "Col size ${col.size} does not match matrix dimension ${rows.size}"
+            )
+        }
+
+        col.forEachIndexed { index, i -> rows[index] = rows[index].plus(i.toDouble()) }
+
+        return this
+    }
+
+    fun addRow(row: IntArray): Matrix {
+        val expectedSize = rows.first().size
+
+        if (row.size != expectedSize) {
+            throw InvalidArgumentException(
+                "Row size ${row.size} does not match matrix dimension $expectedSize"
+            )
+        }
+
+        rows = rows.plus(row.map { it.toDouble() }.toDoubleArray())
+        return this
+    }
+
+    fun plus(m: Matrix): Matrix {
+        if (rows.size != m.rows.size) {
+            throw InvalidArgumentException(
+                "Incompatible matrix dimensions: ${rows.size} vs ${m.rows.size}"
+            )
+        }
+
+        m.rows
+            .forEachIndexed { rowIndex, doubles ->
+                doubles.forEachIndexed { colIndex, d -> rows[rowIndex][colIndex] += d }
+            }
+
+        return this
+    }
+}
---
+++ src/test/kotlin/me/ilex/statistics/util/matrices/MatrixTest.kt
@@ -0,0 +1,35 @@
+package me.ilex.statistics.util.matrices
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class MatrixTest {
+
+    @Test
+    fun `toSingleLine - returns expected data`() {
+        val m = Matrix.makeFromRows(arrayOf(intArrayOf(1, 2), intArrayOf(3, 4)))
+        assertEquals("1.0 2.0 3.0 4.0", m.toSingleLine())
+    }
+
+    @Test
+    fun `addColumn - appends column to the end`() {
+        val m = Matrix.makeFromRows(arrayOf(intArrayOf(1, 2), intArrayOf(3, 4)))
+        m.addColumn(intArrayOf(5, 6))
+        assertEquals("1.0 2.0 5.0 3.0 4.0 6.0", m.toSingleLine())
+    }
+
+    @Test
+    fun `addRow - appends row to the end`() {
+        val m = Matrix.makeFromRows(arrayOf(intArrayOf(1, 2), intArrayOf(3, 4)))
+        m.addRow(intArrayOf(5, 6))
+        assertEquals("1.0 2.0 3.0 4.0 5.0 6.0", m.toSingleLine())
+    }
+
+    @Test
+    fun `plus - returns expected result`() {
+        val m1 = Matrix.makeFromRows(arrayOf(intArrayOf(1, 2)))
+        val m2 = Matrix.makeFromRows(arrayOf(intArrayOf(3, 4)))
+        m1.plus(m2)
+        assertEquals("4.0 6.0", m1.toSingleLine())
+    }
+}
